8b01265043c4a6434b0be3497ad36ab699e29e06
Lalitkkachhwah47@gmail.com <Lalitkkachhwah47@gmail.com>
2019-03-02 18:25:57 +0530

    removed

--- src/main/java/com/example/event/model/EventConstant.java
+++
@@ -1,7 +0,0 @@
-package com.example.event.model;
-
-public class EventConstant {
-
-	public final static String API_ENDPOINT = "api/event/"; 
-	public final static String API_EVENT_BY_LOCATION = API_ENDPOINT+ ""; 
-}
